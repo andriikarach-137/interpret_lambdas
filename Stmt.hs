eb144fca784aaff6a31501de7740f94b7741acdc
Andrii Karach <andrii.karach.13@gmail.com>
2026-08-29 10:29:06 +0100

    Added Stmt module containing ADT of program statements

---
+++ Stmt.hs
@@ -0,0 +1,18 @@
+module Stmt where 
+
+
+import Expr 
+
+
+type Program = [Stmt]
+
+
+data Stmt
+    = Assign String Expr 
+    | Declare String Expr 
+    | While Expr Program 
+    | If Expr Program Program 
+    | Block [Expr] Program 
+    | Print Expr 
+    | PrintF String [Expr]
+    | Input String String  
