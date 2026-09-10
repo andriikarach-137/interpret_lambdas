23d91ae0d21191e463987204bfa46df47352ccec
Andrii Karach <andrii.karach.13@gmail.com>
2026-09-10 14:22:22 +0100

    Added many evaluator functions

--- Eval.hs
+++ Eval.hs
@@ -19,6 +19,7 @@ import Control.Monad (forM_)
 import Value 
 import HM 
 import Data.STRef 
+import Control.Applicative
 
 
 eval :: Env s -> Expr -> ST s (Either Error (Val s))
@@ -41,7 +42,7 @@ evalLit _ (LListEmpty _)     = pure $ Right $ VList []
 evalLit e (LArray a c)       = evalArray e a c 
 evalLit e (LTuple a)         = evalTuple e a 
 evalLit e (LDict ps)         = evalDict e ps 
-evalLit e (LDictEmpty e1 e2) = Right . VDict <$> empty Nothing defaultHash 
+evalLit e (LDictEmpty e1 e2) = Right . VDict <$> HM.empty Nothing defaultHash 
 evalLit _ (LArrow s _ e2)    = pure $ Right $ VArrow s e2   
 
 
@@ -87,14 +88,20 @@ evalDict e ps = do
 
 
 evalUnary :: Env s -> Unary -> Expr -> ST s (Either Error (Val s))
-evalUnary env Neg e    = (eval env e) >>= either (pure . Left) evalNegate
-evalUnary env Not e    = (eval env e) >>= (\re -> pure $ re >>= (\(VBool b) -> Right $ VBool $ not b))  
-evalUnary env ToInt e  = (eval env e) >>= either (pure . Left) evalToInt
-evalUnary env ToReal e = (eval env e) >>= either (pure . Left) evalToReal 
-evalUnary env Fact e   = (eval env e) >>= either (pure . Left) evalFact 
-evalUnary env Len e    = (eval env e) >>= either (pure . Left) evalLen 
-evalUnary env Head e   = (eval env e) >>= either (pure . Left) evalHead 
-evalUnary env Tail e   = (eval env e) >>= either (pure . Left) evalTail 
+evalUnary env Neg e    = evalUnaryOp evalNegate env e 
+evalUnary env Not e    = evalUnaryOp (\(VBool b) -> pure $ Right $ VBool $ not b) env e 
+evalUnary env ToInt e  = evalUnaryOp evalToInt env e
+evalUnary env ToReal e = evalUnaryOp evalToReal env e
+evalUnary env Fact e   = evalUnaryOp evalFact env e
+evalUnary env Len e    = evalUnaryOp evalLen env e
+evalUnary env Head e   = evalUnaryOp evalHead env e
+evalUnary env Tail e   = evalUnaryOp evalTail env e
+
+
+evalUnaryOp :: (Val s -> ST s (Either Error (Val s))) -> Env s -> Expr -> ST s (Either Error (Val s))
+evalUnaryOp op env e = do 
+  re <- eval env e 
+  either (pure . Left) op re  
 
 
 evalNegate :: Val s -> ST s (Either Error (Val s))
@@ -141,3 +148,93 @@ evalTail :: Val s -> ST s (Either Error (Val s))
 evalTail (VList [])     = pure $ Right $ VList []
 evalTail (VList (_:xs)) = pure $ Right $ VList xs 
 evalTail _              = undefined 
+
+
+evalBinary :: Env s -> Binary -> Expr -> Expr -> ST s (Either Error (Val s))
+evalBinary env op e1 e2
+  | op `elem` [Add, Sub, Mul] = evalBinaryOp (evalArithm $ getArithm op) env e1 e2 
+  | op `elem` [Div, Pow]      = evalBinaryOp (evalArithmReal $ getArithmReal op) env e1 e2 
+  | op `elem` [Mod, IntDiv]   = evalBinaryOp (evalArithmInt $ getArithmInt op) env e1 e2   
+  | op `elem` [And, Or, Xor]  = evalBinaryOp (evalBool $ getBool op) env e1 e2 
+  | otherwise                 = undefined
+
+
+evalArithm :: ((Double -> Double -> Either Error Double), (Int -> Int -> Either Error Int)) -> Val s -> Val s -> ST s (Either Error (Val s))
+evalArithm (_, g) (VInt x) (VInt y)   = pure $ VInt  <$> (g x y)
+evalArithm (f, _) (VInt x) (VReal y)  = pure $ VReal <$> (f (fromIntegral x) y)
+evalArithm (f, _) (VReal x) (VInt y)  = pure $ VReal <$> (f x (fromIntegral y))
+evalArithm (f, _) (VReal x) (VReal y) = pure $ VReal <$> (f x y)
+evalArithm _ _ _                      = undefined 
+
+
+getArithm :: (Real a, Real b) => Binary -> ((a -> a -> Either Error a), (b -> b -> Either Error b))
+getArithm Add = (liftBin (+), liftBin (+))
+getArithm Sub = (liftBin (-), liftBin (-))
+getArithm Mul = (liftBin (*), liftBin (*))
+getArithm _   = undefined 
+
+
+evalArithmReal :: (Double -> Double -> Either Error Double) -> Val s -> Val s -> ST s (Either Error (Val s))
+evalArithmReal f (VInt x) (VInt y)   = pure $ VReal <$> (f (fromIntegral x) (fromIntegral y)) 
+evalArithmReal f (VInt x) (VReal y)  = pure $ VReal <$> (f (fromIntegral x) y)
+evalArithmReal f (VReal x) (VInt y)  = pure $ VReal <$> (f x (fromIntegral y))
+evalArithmReal f (VReal x) (VReal y) = pure $ VReal <$> (f x y) 
+evalArithmReal _ _ _                 = undefined 
+
+
+getArithmReal :: Binary -> (Double -> Double -> Either Error Double)
+getArithmReal Div                              = safeDiv 
+  where
+    safeDiv x 0                                = Left $ EvalError ""
+    safeDiv x y                                = Right $ x / y 
+getArithmReal Pow                              = safePow 
+  where
+    safePow x y 
+      | x == 0 && y <= 0                       = Left $ EvalError ""
+      | x < 0 && (x /= fromIntegral (round x)) = Left $ EvalError ""
+      | otherwise                              = Right $ x ** y 
+getArithmReal _                                = undefined 
+
+
+evalArithmInt :: (Int -> Int -> Either Error Int) -> Val s -> Val s -> ST s (Either Error (Val s))
+evalArithmInt f (VInt x) (VInt y) = pure $ VInt <$> f x y 
+evalArithmInt f _ _               = undefined 
+
+
+getArithmInt :: Binary -> (Int -> Int -> Either Error Int)
+getArithmInt Mod    = safeMod 
+  where
+    safeMod m n 
+      | n == 0      = Left $ EvalError ""
+      | otherwise   = Right $ mod m n 
+getArithmInt IntDiv = safeIntDiv 
+  where
+    safeIntDiv m n 
+      | n == 0      = Left $ EvalError ""
+      | otherwise   = Right $ div m n 
+
+
+evalBool :: (Bool -> Bool -> Either Error Bool) -> Val s -> Val s -> ST s (Either Error (Val s))
+evalBool f (VBool p) (VBool q) = pure $ VBool <$> f p q  
+evalBool _ _ _                 = undefined 
+
+
+getBool :: Binary -> (Bool -> Bool -> Either Error Bool)
+getBool And = liftBin (&&)
+getBool Or  = liftBin (||)
+getBool Xor = liftBin (/=)
+
+
+liftBin :: (a -> a -> a) -> (a -> a -> Either Error a)
+liftBin f = \x y -> pure $ f x y
+
+
+evalBinaryOp :: (Val s -> Val s -> ST s (Either Error (Val s))) -> Env s -> Expr -> Expr -> ST s (Either Error (Val s))
+evalBinaryOp op env e1 e2 = do 
+  re1 <- eval env e1 
+  re2 <- eval env e2 
+  case (re1, re2) of 
+    err@(Left err1, Left err2) -> pure $ fst err <|> snd err 
+    (Left err, Right _)        -> pure $ Left err
+    (Right _, Left err)        -> pure $ Left err 
+    (Right ve1, Right ve2)     -> op ve1 ve2  
